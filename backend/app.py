e7eccbfad3ce00be1d5607d8b83370e306640d16
Gaurang Rane <80737386+gaurane@users.noreply.github.com>
2025-03-14 22:21:18 +0530

    app.py

--- backend/app.py
+++ backend/app.py
@@ -30,4 +30,5 @@ def predict():
     return jsonify({"prediction": prediction.tolist()})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
